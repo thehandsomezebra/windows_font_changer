96aac353eba27c59eee2011d7b00dc69f2cc144b
Stephanie Lage Frantz <46173415+thehandsomezebra@users.noreply.github.com>
2022-03-28 09:27:43 -0400

    Update new_font.bat
    changes: 1 - reset it as step one.  2. user prompt for whatever font you want.

--- new_font.bat
+++ new_font.bat
@@ -1,3 +1,37 @@
+:: reverting.. just to keep a baseline.
+
+echo Windows Registry Editor Version 5.00 > "c:/revertfont.reg"
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts] >> "c:/revertfont.reg"
+echo "Segoe UI (TrueType)"="segoeui.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Black (TrueType)"="seguibl.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Black Italic (TrueType)"="seguibli.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Bold (TrueType)"="segoeuib.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Bold Italic (TrueType)"="segoeuiz.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Emoji (TrueType)"="seguiemj.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Historic (TrueType)"="seguihis.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Italic (TrueType)"="segoeuii.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Light (TrueType)"="segoeuil.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Light Italic (TrueType)"="seguili.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Semibold (TrueType)"="seguisb.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Semibold Italic (TrueType)"="seguisbi.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Semilight (TrueType)"="segoeuisl.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Semilight Italic (TrueType)"="seguisli.ttf" >> "c:/revertfont.reg"
+echo "Segoe UI Symbol (TrueType)"="seguisym.ttf" >> "c:/revertfont.reg"
+echo "Segoe MDL2 Assets (TrueType)"="segmdl2.ttf" >> "c:/revertfont.reg"
+echo "Segoe Print (TrueType)"="segoepr.ttf" >> "c:/revertfont.reg"
+echo "Segoe Print Bold (TrueType)"="segoeprb.ttf" >> "c:/revertfont.reg"
+echo "Segoe Script (TrueType)"="segoesc.ttf" >> "c:/revertfont.reg"
+echo "Segoe Script Bold (TrueType)"="segoescb.ttf" >> "c:/revertfont.reg"
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes] >> "c:/revertfont.reg"
+echo "Segoe UI"=- >> "c:/revertfont.reg"
+
+
+REGEDIT c:/revertfont.reg
+
+del c:/revertfont.reg
+
+:: new type
+
 echo Windows Registry Editor Version 5.00 >"c:/newfont.reg"
 echo  [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts] >>"c:/newfont.reg"
 echo  "Segoe UI (TrueType)"="" >>"c:/newfont.reg"
@@ -8,8 +42,12 @@ echo  "Segoe UI Light (TrueType)"="" >>"c:/newfont.reg"
 echo  "Segoe UI Semibold (TrueType)"="" >>"c:/newfont.reg"
 echo  "Segoe UI Symbol (TrueType)"="" >>"c:/newfont.reg"
 echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes] >>"c:/newfont.reg"
-echo "Segoe UI"="Verdana Bold">>"c:/newfont.reg"
 
+:: echo "Segoe UI"="Verdana Bold">>"c:/newfont.reg"
+
+@echo off
+set /p NewFontChoice=Please type the name of the font you want... then hit ENTER
+echo "Segoe UI"=%NewFontChoice% >>"c:/newfont.reg"
 
 REGEDIT c:/newfont.reg
 
